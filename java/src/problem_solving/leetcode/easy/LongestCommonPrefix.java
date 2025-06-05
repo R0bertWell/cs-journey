8cb3510f359d252ac811a5ff21907a659d29f8e5
r0bertwell <r0bertwell@hotmail.com>
2025-06-05 11:17:31 -0300

    [QUESTION][SOLVED][ENHANCED] - LongestCommonPrefix.

--- java/src/problem_solving/leetcode/easy/LongestCommonPrefix.java
+++ java/src/problem_solving/leetcode/easy/LongestCommonPrefix.java
@@ -21,6 +21,22 @@ public class LongestCommonPrefix {
         }
     }
 
+    public static String longestCommonPrefixEnhanced(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            // Reduz o prefixo até encontrar correspondência
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+
     public static void main(String[] args) {
         System.out.println(longestCommonPrefix(new String[]{"a","ac"}));
         System.out.println(longestCommonPrefix(new String[]{"cir","car"}));
